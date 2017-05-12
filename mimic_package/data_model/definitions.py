905d89aa96ae3d4fbfb4a4911d902b7b95eaa2d8
Matt Lewis <mattrlewis2@gmail.com>
2017-05-12 10:45:21 -0700

    code cleanup model / definitions / mapper

--- mimic_package/data_model/definitions.py
+++ mimic_package/data_model/definitions.py
@@ -108,96 +108,11 @@ insurance_values = collections.OrderedDict(insurance_status_dict)
 
 
 '''
-Charleston comorbidity dicts
+Charleston comorbidity dicts | 'enhanced ICD-9 CM' 
 Based off of http://czresearch.com/dropbox/Quan_MedCare_2005v43p1130.pdf
 # note that as of MIMIC III 1.0 there are only ICD9 codes... 
 '''
 
-
-# elixhauser_icd9_coding = {
-#                             'congestive heart failure': ['398.91', '402.01', '402.11', '402.91', '404.01', 
-#                                                          '404.03', '404.11', '404.13', '404.91', '404.93', 
-#                                                          '425.4-425.9', '428.x'],
-#                             'cardiac arrhythmias': ['426.0', '426.13', '426.7', '426.9', '426.10', 
-#                                                     '426.12', '427.0-427.4', '427.6-427.9', '785.0', '996.01', 
-#                                                     '996.04', 'V45.0', 'V53.3'],
-#                             'valvular disease': ['093.2', '394.x-397.x', '424.x', '746.3-746.6', 'V42.2', 'V43.3'],
-#                             'pulmonary circulation disorders': ['415.0', '415.1', '416.x', '417.0', '417.8', '417.9'],
-#                             'peripheral vascular disorders': ['093.0', '437.3', '440.x', '441.x', '443.1-443.9', 
-#                                                               '447.1', '557.1', '557.9', 'V43.4'],
-#                             'hypertension, uncomplicated ': ['401.x'],
-#                             'hypertension, complicated': ['402.x-405.x'],
-#                             'paralysis': ['334.1', '342.x', '343.x', '344.0-344.6', '344.9'], 
-#                             'other neurological disorders': ['331.9', '332.0', '332.1', '333.4', '333.5', '333.92', 
-#                                                              '334.x-335.x', '336.2', '340.x', '341.x', '345.x', 
-#                                                              '348.1', '348.3', '780.3', '784.3'],
-#                             'chronic pulmonary disease': ['416.8', '416.9', '490.x-505.x', '506.4', '508.1', '508.8'],
-#                             'diabetes, uncomplicated': ['250.0-250.3'],
-#                             'diabetes, complicated': ['250.4-250.9'],
-#                             'hypothyroidism': ['240.9', '243.x', '244.x', '246.1', '246.8'],
-#                             'renal failure': ['403.01', '403.11', '403.91', '404.02',
-#                                               '404.03', '404.12', '404.13', '404.92', '404.93', 
-#                                               '585.x', '586.x', '588.0', 'V42.0', 'V45.1', 'V56.x'],
-#                             'liver disease': ['070.22', '070.23', '070.32', '070.33', '070.44', '070.54', '070.6', 
-#                                               '070.9', '456.0-456.2', '570.x', '571.x', '572.2-572.8', 
-#                                               '573.3', '573.4', '573.8', '573.9',' V42.7'] ,
-#                             'peptic ulcer disease excluding bleeding': ['531.7', '531.9', '532.7', '532.9', '533.7', 
-#                                                                         '533.9', '534.7', '534.9'],
-#                             'aids/hiv': ['042.x-044.x'], 
-#                             'lymphoma': ['200.x-202.x', '203.0', '238.6'],
-#                             'metastatic cancer': ['196.x-199.x'],
-#                             'solid tumor without metastasis': ['140.x-172.x',' 174.x-195.x'],
-#                             'rheumatoid arthritis/collagen vascular diseases': ['446.x', '701.0', '710.0-710.4',' 710.8', '710.9',
-#                                                                                 '711.2', '714.x', '719.3', '720.x', '725.x', '728.5',
-#                                                                                 '728.89', '729.30'],
-#                             'coagulopathy': ['286.x', '287.1', '287.3-287.5'], 
-#                             'obesity': ['278.0'],
-#                             'weight loss': ['260.x-263.x', '783.2', '799.4'], 
-#                             'fluid and electrolyte disorders': ['253.6', '276.x'], 
-#                             'blood loss anemia': ['280.0'],
-#                             'deficiency anemia': ['280.1-280.9', '281.x'], 
-#                             'alcohol abuse': ['265.2', '291.1-291.3', '291.5-291.9', '303.0', '303.9', '305.0', 
-#                                               '357.5', '425.5', '535.3', '571.0-571.3', '980.x', 'V11.3'],
-#                             'drug abuse': ['292.x', '304.x', '305.2-305.9', 'V65.42'],
-#                             'psychoses': ['293.8', '295.x', '296.04', '296.14', '296.44', '296.54', '297.x', '298.x'],
-#                             'depression': ['296.2', '296.3', '296.5', '300.4', '309.x', '311']
-#     } 
-# 
-# elixhauser_values = {
-#                             'congestive heart failure': 0, 
-#                             'cardiac arrhythmias': 0, 
-#                             'valvular disease': 0, 
-#                             'pulmonary circulation disorders': 0, 
-#                             'peripheral vascular disorders': 0, 
-#                             'hypertension, uncomplicated ': 0, 
-#                             'hypertension, complicated': 0, 
-#                             'paralysis': 0, 
-#                             'other neurological disorders': 0, 
-#                             'chronic pulmonary disease': 0, 
-#                             'diabetes, uncomplicated': 0, 
-#                             'diabetes, complicated': 0, 
-#                             'hypothyroidism': 0, 
-#                             'renal failure': 0, 
-#                             'liver disease': 0,
-#                             'peptic ulcer disease excluding bleeding': 0, 
-#                             'aids/hiv': 0, 
-#                             'lymphoma': 0, 
-#                             'metastatic cancer': 0, 
-#                             'solid tumor without metastasis': 0, 
-#                             'rheumatoid arthritis/collagen vascular diseases': 0, 
-#                             'coagulopathy': 0, 
-#                             'obesity': 0, 
-#                            ' weight loss': 0, 
-#                             'fluid and electrolyte disorders': 0, 
-#                             'blood loss anemia': 0, 
-#                             'deficiency anemia': 0, 
-#                             'alcohol abuse': 0, 
-#                             'drug abuse': 0, 
-#                             'psychoses': 0, 
-#                             'depression': 0
-#     } 
-
-# from http://czresearch.com/dropbox/Quan_MedCare_2005v43p1130.pdf | 'enhanced ICD-9 CM' 
 updated_charlson_icd9_coding = {
                             'Myocardial infarction': ['410.x', '412.x'],
                             'Congestive heart failure': ['398.91', '402.01', '402.11', '402.91',
@@ -231,7 +146,6 @@ updated_charlson_icd9_coding = {
                             } 
 
 # from https://academic.oup.com/aje/article/173/6/676/182985/Updating-and-Validating-the-Charlson-Comorbidity
-
 updated_charlson_weights = {
                             'Myocardial infarction': 0,
                             'Congestive heart failure': 2,
@@ -253,14 +167,12 @@ updated_charlson_weights = {
                             }
 
 charlson_features = {k: 0 for k in updated_charlson_icd9_coding.keys()}
-
 index_admission_mapper = {"EMERGENCY": 0, "URGENT":0, "ELECTIVE":0}
 index_admission_types = collections.OrderedDict(index_admission_mapper)
 
 
 '''
 ICD9 parsing functions
-- will move somewhere else
 '''
 
 def fill_until(code):
@@ -323,6 +235,11 @@ def parse_icd_code(code):
         codes.add(prefix+s_code)
     return codes
 
+
+'''
+Charlson / CCS matching functions
+'''
+
 def return_charlson_score(user_charlson_features):
     categories = {k:v for (k,v) in user_charlson_features.iteritems() if v > 0}
     score = 0
@@ -332,8 +249,7 @@ def return_charlson_score(user_charlson_features):
     return score
         
 
-def check_against_charlson(user_codes, codes_dict=updated_charlson_icd9_coding): # won't be none
-    
+def check_against_charlson(user_codes, codes_dict=updated_charlson_icd9_coding): 
     charlson_features = {k: 0 for k in updated_charlson_icd9_coding.keys()}
     expanded_codes = {} 
     for key, value in codes_dict.iteritems(): 
@@ -354,13 +270,16 @@ def check_against_charlson(user_codes, codes_dict=updated_charlson_icd9_coding):
 
 
 def check_against_ccs(user_codes, codeset, code_type='dx', code_level='single'):
+    '''
+    See CCS documentation for reference on code_level definitions: 
+    https://www.hcup-us.ahrq.gov/toolssoftware/ccs/CCSUsersGuide.pdf
+    '''
     
     mapper = {'dx': {'single': codeset.dx_single_level_codes, 'category': codeset.dx_category_level_codes, 'multi': codeset.dx_multilevel_codes}, 
               'px': {'single': codeset.px_single_level_codes, 'category': codeset.px_category_level_codes, 'multi': codeset.px_multilevel_codes}}
     
     f = mapper[code_type][code_level]
     
-
     ccs_features = {} 
     user_set = set(user_codes)
     for k,v in f.iteritems():
